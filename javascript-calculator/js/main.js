cf28574fa470a05f2a2313dd99a2dcb515ffa692
shussain03 <46450877+shussain03@users.noreply.github.com>
2019-01-20 21:07:31 +0000

    updated code

---
+++ javascript-calculator/js/main.js
@@ -0,0 +1,33 @@
+/* brief design a basic and advance calculator.
+The calculator should do the following:
+have Basic (+, -, *, /) and Advanced (BMI calculator and Celcius to Farenheit Conversion) calculators
+Use prompts() to get user input and alerts to display messages and results
+Use functions to do the calculations
+Use control flow (if/else and switch) to direct the user
+Keep running until the User chooses not to (while loop)*/
+
+// console.log("working");
+
+// var useCalculator = true;
+
+// while (useCalculator) {
+//   console.log("calculating...");
+//   //
+//   var typeCalculator = promp("Would you like to use Basic or Advance calculator?" + (
+//     break) + "Type Basic or Advance.")
+//   if (basicCalc === "Basic.") {
+//     or
+//     else(advanceCalc === "Advance.")
+//   }
+
+//   // Stop calculating
+//   var nextCalc = prompt("Would you like to continue?");
+//   if (nextCalc === "no") {
+//     useCalculator = false
+//   }
+// }
+
+//create a prompt
+//give user option to choose basic and advance calculator option
+//ask user if they want to go ahead with the calculation
+//
---
+++ javascript-calculator/js/page2.js
@@ -0,0 +1,31 @@
+// console.log("working");
+
+// /* This script displays a greeting to the user based on the current time.*/
+
+// var greeting = (function () {
+//       var today = new Date(); //create a new date object
+//       var hourNow = today.getHours(); //find the current hour
+//       var greeting = greetUser();
+
+//       /* Displays a greeting based on the current time.*/
+
+//       function greetUser() {
+//         if (hourNow > 20) {
+//           greeting = "Good evening";
+//         } else if (hourNow > 12) {
+//           greeting = "Good afternoon";
+//         } else if (hourNow > 0) {
+//           greeting = "Good morning";
+//         } else {
+//           greeting = "Welcome";
+//         }
+//         return greeting + getName();
+
+//         function getName() {
+//           var name = 'Molly';
+//           return name;
+//         }
+//       };
+//       alert(greetUser):
+
+//         console.log("greeting");
